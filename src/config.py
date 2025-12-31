a684762f4d55546ad6f70e850f509013d0e0fb28
Ninghui Tu <116436632+SkyesProphet@users.noreply.github.com>
2025-12-31 22:15:51 +0800

    Add LABELS_DEFINITION for topic labels
    Define a dictionary for topic labels in config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# topic label define
+LABELS_DEFINITION = {
+# Here are the predefined attribute labels saved.
+# Please save as a dictionary format.
+# The attribute labels must align with the content of the dataset.
+}
